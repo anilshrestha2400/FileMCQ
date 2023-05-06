7b51258ec93e19a822b2bd0cf123eec853d5cdaf
anilshrestha2400 <anilcrest2400@gmail.com>
2023-05-06 20:47:55 +0545

    demo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch("/questions.txt")
+      .then((response) => response.text())
+      .then((data) => {
+        const lines = data.trim().split("\n");
+        const qs = [];
+        let currentQuestion = {};
+        for (let i = 0; i < lines.length; i++) {
+          const line = lines[i].trim();
+          if (line.startsWith("Question")) {
+            if (currentQuestion.question) qs.push(currentQuestion);
+            currentQuestion = {
+              question: line.substring(line.indexOf(":") + 2),
+            };
+          } else if (line.startsWith("Answer")) {
+            currentQuestion.answer = line.substring(line.indexOf(":") + 2);
+          } else if (line.startsWith("A:")) {
+            currentQuestion.options = [];
+            currentQuestion.options.push(line.substring(line.indexOf(":") + 2));
+          } else if (line.startsWith("B:")) {
+            currentQuestion.options.push(line.substring(line.indexOf(":") + 2));
+          } else if (line.startsWith("C:")) {
+            currentQuestion.options.push(line.substring(line.indexOf(":") + 2));
+          } else if (line.startsWith("D:")) {
+            currentQuestion.options.push(line.substring(line.indexOf(":") + 2));
+          }
+        }
+        if (currentQuestion.question) qs.push(currentQuestion);
+        setQuestions(qs);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+  console.log(questions);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [selectedOption, setSelectedOption] = useState("");
+  const [score, setScore] = useState(0);
+  console.log(selectedOption);
+  const handleOptionChange = (event) => setSelectedOption(event.target.value);
+
+  const handleNextClick = () => {
+    if (selectedOption === questions[currentQuestionIndex].answer) {
+      setScore(score + 1);
+    }
+    setSelectedOption("");
+    setCurrentQuestionIndex(currentQuestionIndex + 1);
+  };
+
+  if (!questions.length) return <div>Loading questions...</div>;
+
+  if (currentQuestionIndex >= questions.length) {
+    return (
+      <div>
+        <h2>
+          Your score: {score} out of {questions.length}
+        </h2>
+      </div>
+    );
+  }
+
+  const currentQuestion = questions[currentQuestionIndex];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ques">
+      <h2>{currentQuestion.question}</h2>
+      {currentQuestion.options.map((option, index) => (
+        <div key={index} className="radio">
+          <input
+            type="radio"
+            name="option"
+            value={option}
+            checked={selectedOption === option}
+            onChange={handleOptionChange}
+            required
+          />
+          <label>{option}</label>
+        </div>
+      ))}
+      <div className="bookdiv">
+        <button onClick={handleNextClick} className="bookbtn">
+          Next
+        </button>
+      </div>
     </div>
   );
 }
